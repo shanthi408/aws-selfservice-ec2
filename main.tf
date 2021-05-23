bcfb00cfb86772599262a1de7acad20047f06bd4
shanthi408 <shanthi408@gmail.com>
2021-05-23 15:10:42 -0500

    adding parameter env

--- main.tf
+++ main.tf
@@ -12,13 +12,13 @@ terraform {
 }
 
 provider "aws" {
-  region = "${var.region}"
+  region = "${TF_VAR_region}"
 }
 
 
 module "ec2" {
   source                      = "./modules/ec2"
-  region                      = "${var.region}"
+  region                      = "${TF_VAR_region}"
   #ec2_host_key_pair           = var.ec2_host_key_pair
   ec2_ami                     = var.ec2_ami
   kms_key_id                  = var.kms_key_id
