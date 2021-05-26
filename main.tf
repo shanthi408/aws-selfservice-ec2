a12ddbad0477b571da90fd5b2dca2f8016cdd476
shanthi408 <shanthi408@gmail.com>
2021-05-26 08:54:59 -0500

    Make file

--- main.tf
+++ main.tf
@@ -8,12 +8,18 @@ terraform {
     key     = "${var.appname}.tfstate"
     dynamodb_table = "abc-343-${var.environment}-${var.region}-167-terraform"
 
+    workspaces {
+      name = "ec2-${var.environment}-${var.region}"
+    }
+
   }
 }
 
 provider "aws" {
   region = var.region
 }
+
+
 
 
 module "ec2" {
