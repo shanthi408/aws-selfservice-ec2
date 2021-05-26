80209bb146e1b8931d68a2b93770774920e86aff
shanthi408 <shanthi408@gmail.com>
2021-05-26 00:00:46 -0500

    adding

--- main.tf
+++ main.tf
@@ -4,9 +4,9 @@ terraform {
   backend "s3" {
     encrypt = true
     acl     = "private"
-    bucket  = "abc-343-"${var.environment}"-"${var.region}"-167-terraform"
+    bucket  = "abc-343-${var.environment}-${var.region}-167-terraform"
     key     = "${var.appname}.tfstate"
-    dynamodb_table = "abc-343-"${var.environment}"-"${var.region}"-167-terraform"
+    dynamodb_table = "abc-343-${var.environment}-${var.region}-167-terraform"
 
   }
 }
